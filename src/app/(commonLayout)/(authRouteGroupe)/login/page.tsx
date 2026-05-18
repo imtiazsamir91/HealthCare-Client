e04b04190d4c7e2519e40bd57bda3c4aa2f381ed
Imtiaz Samir <imtiazsamir91@gmail.com>
2026-05-18 19:12:57 +0600

    dashboard

--- src/app/(commonLayout)/(authRouteGroupe)/login/page.tsx
+++ src/app/(commonLayout)/(authRouteGroupe)/login/page.tsx
@@ -1,10 +1,15 @@
-import LoginForm from '@/components/modules/Auth/loginForm'
-import React from 'react'
+import LoginForm from "@/components/modules/Auth/loginForm";
 
-const Loginpage = () => {
+interface LoginParams {
+  searchParams: Promise<{ redirect?: string }>;
+}
+
+const LoginPage = async ({ searchParams }: LoginParams) => {
+  const params = await searchParams;
+  const redirectPath = params.redirect;
   return (
-   <LoginForm />
+    <LoginForm redirectPath={redirectPath}/>
   )
 }
 
-export default Loginpage
+export default LoginPage
